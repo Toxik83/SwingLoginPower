e6bc2d1780db5e2a573a9640d6f1127303d7adb1
USER <USER@192.168.1.3>
2014-10-13 16:46:13 +0300

    A positioned GUI

--- SwingLoginPower/src/PowerGUI.java
+++ SwingLoginPower/src/PowerGUI.java
@@ -12,9 +12,12 @@ public class PowerGUI extends JFrame {
 	private JButton powerButton;
 	private JLabel numberLabel;
 	private JLabel exponentLabel;
-    private JLabel resultLabel;
-    private JTextField result;
+	private JLabel resultLabel;
+	private JTextField result;
+
 	public PowerGUI() {
+		
+
 		setVisible(true);
 		setLayout(new FlowLayout());
 		numberLabel = new JLabel("Number");
@@ -25,38 +28,36 @@ public class PowerGUI extends JFrame {
 		add(exponentLabel);
 		exponent = new JTextField(10);
 		add(exponent);
-		powerButton =new JButton("Power");
+		powerButton = new JButton("Power");
 		add(powerButton);
-		resultLabel=new JLabel("Result is:");
+		resultLabel = new JLabel("Result is:");
 		add(resultLabel);
 		result = new JTextField(10);
 		add(result);
-		powerButton.addActionListener(new ActionListener(){
+		powerButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				// TODO Auto-generated method stub
 				double num;
 				int exponent2;
-				try{
-					num=Double.parseDouble(number.getText());
-					exponent2=Integer.parseInt(exponent.getText());
-					double answer=1.0;
-					for (int i=0;i<exponent2;i++){
-						answer *=num;
+				try {
+					num = Double.parseDouble(number.getText());
+					exponent2 = Integer.parseInt(exponent.getText());
+					double answer = 1.0;
+					for (int i = 0; i < exponent2; i++) {
+						answer *= num;
 						result.setText(Double.toString(answer));
 					}
-				}catch(Exception e){
-					JOptionPane.showMessageDialog(null,"Please enter numbers");
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "Please enter numbers");
 				}
 			}
-			
+
 		});
-
+		pack();
+		setLocationRelativeTo(null);
+		
 	}
 
-	
-
-	}
-
-
+}
